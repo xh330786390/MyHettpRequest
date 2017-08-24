123b0a67c688ba040bb8600d21b5a957cba51aa5
xh330786390 <330786390@qq.com>
2017-08-24 14:45:05 +0800

    添加测试项目

---
+++ MyHttpRequest.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyHettpRequest;
+
+namespace MyHttpRequest.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = HttpHelper.HttpGetConnectToServer("http://192.168.20.234:9090", "");
+            Console.Read();
+            //192.168.20.234:9090
+        }
+    }
+}
